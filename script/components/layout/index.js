48e24a5a3f9b0e229be089ecb713b23c8fcda81e
Gregor Anders <gregor.anders@gmail.com>
2020-05-29 01:25:23 +0200

    WIP

--- script/components/layout/index.js
+++ script/components/layout/index.js
@@ -1,10 +1,1 @@
-import React, { memo } from 'react';
-import { element as IsElement } from 'prop-types';
-export const Layout = ({ children }) => {
-    return React.createElement("main", null, children);
-};
-Layout.displayName = 'Layout';
-Layout.propTypes = {
-    children: IsElement.isRequired,
-};
-export default memo(Layout);
+import e,{memo as t}from"react";import{element as u}from"prop-types";export const Layout=({children:t})=>e.createElement("main",null,t);Layout.displayName="Layout",Layout.propTypes={children:u.isRequired};export default t(Layout);
--- script/components/pages/world/index.js
+++ script/components/pages/world/index.js
@@ -1,12 +1,1 @@
-import React, { memo, Suspense } from 'react';
-import { useRecoilValue } from 'recoil';
-import { Selector } from 'states/model';
-import Loading from 'loading';
-import Countries from 'components/organisms/countries';
-export const World = () => {
-    const models = useRecoilValue(Selector);
-    return (React.createElement(Suspense, { fallback: React.createElement(Loading, null) },
-        React.createElement(Countries, { models: models })));
-};
-World.displayName = 'World';
-export default memo(World);
+import e,{memo as t,Suspense as j}from"react";import{useRecoilValue as w}from"recoil";import{Selector as z}from"states/model";import D from"loading";import W from"components/organisms/countries";export const World=()=>{const o=w(z);return e.createElement(j,{fallback:e.createElement(D,null)},e.createElement(W,{models:o}))};World.displayName="World";export default t(World);
